6d7436a097d6bd1a28e13622da30bd5c25fc0db3
made-studio <matteusengstr.m@gmail.com>
2024-01-16 22:09:10 +0100

    first commit

--- music crawler/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("text/plain");
-$conn = new mysqli("p:localhost", "root", "Matteus@13", "playlist");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-?>
